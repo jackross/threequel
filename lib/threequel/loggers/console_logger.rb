9c406e56e42a6627621e81453123257757d4f166
Jack Ross <jack.ross@technekes.com>
2013-05-08 09:10:03 -0400

    adds rows_affected output to console logging

--- lib/threequel/loggers/console_logger.rb
+++ lib/threequel/loggers/console_logger.rb
@@ -1,17 +1,18 @@
 module Threequel
   class ConsoleLogger
+    attr_reader :default_attributes
 
     def initialize
-      @attributes = {}
+      @default_attributes = {}
     end
 
     def log(stage, attributes)
-      @attributes.merge!(attributes)
+      attributes.merge!(default_attributes)
       puts case stage
         when :executing
-          "-- Starting execution of #{@attributes[:name]} at #{@attributes[:started_at]}\n"
+          "-- Starting execution of #{attributes[:name]} at #{attributes[:started_at]}\n"
         when :finished
-          "-- Finishing execution of #{@attributes[:name]} at #{@attributes[:finished_at]} in #{@attributes[:duration]} seconds"
+          "-- Finishing execution of #{attributes[:name]} at #{attributes[:finished_at]} in #{attributes[:duration]} seconds (#{attributes[:rows_affected]} rows affected)"
       end
     end
 
