0a9af1e33ec78ca82cb87006da1f7d3eaafacb1c
Zach Blankenship <zach.blankenship@technekes.com>
2014-04-02 15:31:03 -0400

    removes executioner module and reformats

--- lib/threequel/executioner/config_loader.rb
+++ lib/threequel/executioner/config_loader.rb
@@ -1,33 +1,31 @@
 module Threequel
-  module Executioner
-    class ConfigLoader
-      def initialize(folder_path)
-        @folder_path = folder_path
-      end
+  class ConfigLoader
+    def initialize(folder_path)
+      @folder_path = folder_path
+    end
 
-      def dependency_tree
-        @dependency_tree ||= {}.tap{|h| scripts.each{|k, v| h[k] = v[:dependencies]}}
-      end
+    def dependency_tree
+      @dependency_tree ||= {}.tap{|h| scripts.each{|k, v| h[k] = v[:dependencies]}}
+    end
 
-      def sorter
-        @sorter ||= Sorter.new(dependency_tree)
-      end
+    def sorter
+      @sorter ||= Sorter.new(dependency_tree)
+    end
 
-      def scripts
-        config[:scripts]
-      end
+    def scripts
+      config[:scripts]
+    end
 
-      def script_chains
-        @script_chains ||= sorter.chains
-      end
+    def script_chains
+      @script_chains ||= sorter.chains
+    end
 
-      def tsorted_scripts
-        @tsorted_scripts ||= sorter.tsorted
-      end
+    def tsorted_scripts
+      @tsorted_scripts ||= sorter.tsorted
+    end
 
-      def config
-        @config ||= YAML.load_file(File.join(Rails.root, "db", "sql", @folder_path, "config.yml")).with_indifferent_access
-      end
+    def config
+      @config ||= YAML.load_file(File.join(Rails.root, "db", "sql", @folder_path, "config.yml")).with_indifferent_access
     end
   end
 end
--- lib/threequel/executioner/script.rb
+++ lib/threequel/executioner/script.rb
@@ -1,30 +1,27 @@
 module Threequel
-  module Executioner
-    class Script
-      attr_accessor :code
-      attr_accessor :path
-      attr_accessor :file_name
+  class Script
+    attr_accessor :code
+    attr_accessor :path
+    attr_accessor :file_name
 
-      def initialize(path)
-        @path = path
-      end
+    def initialize(path)
+      @path = path
+    end
 
-      def file_name
-        @file_name ||= "#{File.join(Rails.root, 'db', 'sql', @path)}.sql"
-      end
+    def file_name
+      @file_name ||= "#{File.join(Rails.root, 'db', 'sql', @path)}.sql"
+    end
 
-      def code
-        @code ||= IO.read(file_name).gsub(/\r\n?/, "\n")
-      end
+    def code
+      @code ||= IO.read(file_name).gsub(/\r\n?/, "\n")
+    end
 
-      def statements
-        code.split("GO\n").compact
-      end
+    def statements
+      code.split("GO\n").compact
+    end
 
-      def sql
-        statements.join("\n")
-      end
-
+    def sql
+      statements.join("\n")
     end
   end
 end
